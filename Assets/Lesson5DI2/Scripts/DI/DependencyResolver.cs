a1033758efd2672c7b1faf33503105290c158e6f
Pavel Gordienko <guzmo6@rambler.ru>
2023-05-28 13:26:19 +0300

    add recursive Inject

--- Assets/Lesson5DI2/Scripts/DI/DependencyResolver.cs
+++ Assets/Lesson5DI2/Scripts/DI/DependencyResolver.cs
@@ -4,20 +4,37 @@ namespace Lesson5DI2
 {
     public sealed class DependencyResolver : MonoBehaviour
     {
-        [SerializeField] private CameraFollower _cameraFollower;
-        [SerializeField] private MoveController _moveController;
+        // [SerializeField] private CameraFollower _cameraFollower;
+        // [SerializeField] private MoveController _moveController;
 
         // init на gameManager вызывать не нужно
         private void Start()
         {
-            DependencyInjector.Inject(_cameraFollower);
-            DependencyInjector.Inject(_moveController);
+            ResolveDependencies(transform);
+            
+            // DependencyInjector.Inject(_cameraFollower);
+            // DependencyInjector.Inject(_moveController);
             
             // var playerService = ServiceLocator.GetService<IPlayerService>();
             // var keyboardInput = ServiceLocator.GetService<KeyboardInput>();
             //
             // _cameraFollower.Construct(playerService);
             // _moveController.Construct(keyboardInput, playerService); 
+        }
+
+        private void ResolveDependencies(Transform node)
+        {
+            var behaviours = node.GetComponents<MonoBehaviour>();
+
+            foreach (var behaviour in behaviours)
+            {
+                DependencyInjector.Inject(behaviour);
+            }
+
+            foreach (Transform child in node)
+            {
+                ResolveDependencies(child);
+            }
         }
     }
 }
