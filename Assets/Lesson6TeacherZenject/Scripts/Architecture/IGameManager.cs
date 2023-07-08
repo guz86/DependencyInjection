32941cd817ca0a507266eb0498542cfd3025b68c
Pavel Gordienko <guzmo6@rambler.ru>
2023-07-08 12:41:58 +0300

    add IGameManager add SceneInstaller zenject

---
+++ Assets/Lesson6TeacherZenject/Scripts/Architecture/IGameManager.cs
@@ -0,0 +1,15 @@
+﻿namespace Lesson6TeacherZenject.Scripts.Architecture
+{
+    public interface IGameManager
+    {
+        public GameState CurrentState { get; }
+        
+        public void AddListener(IGameListener listener);
+        
+        public void InitializeGame();
+
+        public void StartGame();
+
+        public void FinishGame();
+    }
+}
---
+++ Assets/Lesson6TeacherZenject/Scripts/Zenject/SceneInstaller.cs
@@ -0,0 +1,21 @@
+﻿using Lesson6TeacherZenject.Scripts.Architecture;
+using UnityEngine;
+using Zenject;
+
+namespace Lesson6TeacherZenject.Scripts.Zenject
+{
+    public sealed class SceneInstaller : MonoInstaller<SceneInstaller>
+    {
+        //[SerializeField] private GameManager _gameManager;
+        
+        
+        public override void InstallBindings()
+        {
+            Container.Bind<GameManagerContext>().AsSingle();
+            
+            //Container.Bind<IGameManager>().To<GameManager>().FromInstance(_gameManager).AsSingle();
+            //Container.Bind<IGameManager>().FromInstance(_gameManager).AsSingle();
+            Container.Bind<IGameManager>().FromComponentInHierarchy().AsSingle();
+        }
+    }
+}
